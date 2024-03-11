7db8a1fbc051e2c8614495e2b2e485e6aeeef407
S.Siva Sri Varshini <115179509+SIVASRI-35@users.noreply.github.com>
2024-03-11 10:36:51 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import streamlit.components.v1 as components
+ 
+
+components.html(
+    """
+    <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+    <df-messenger
+        chat-title="AIClub"
+        agent-id="3cb41952-578d-43df-a37d-f1f12bcc12e1"
+        language-code="en"></df-messenger>
+    """,
+    height=700, # try various values to see what works best (maybe use st.slider)
+)
