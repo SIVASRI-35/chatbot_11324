0013409b720de437c5b965e58a70661a85063ac2
S.Siva Sri Varshini <115179509+SIVASRI-35@users.noreply.github.com>
2024-03-12 10:05:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ components.html(
         agent-id="3cb41952-578d-43df-a37d-f1f12bcc12e1"
         language-code="en"></df-messenger>
     """,
-    height=700, # try various values to see what works best (maybe use st.slider)
+    height=400, # try various values to see what works best (maybe use st.slider)
 
 )
 #st.markdown(hide_github_icon, unsafe_allow_html=True)
