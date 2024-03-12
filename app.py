6cb02855364f0d03a2a1e28d4740181bf9c0d49e
S.Siva Sri Varshini <115179509+SIVASRI-35@users.noreply.github.com>
2024-03-12 10:05:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 import streamlit as st
 import streamlit.components.v1 as components
  
-hide_github_icon = "#GithubIcon {  visibility: hidden;}"
+
 
 
 components.html(
     """
     <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+    <style>#GithubIcon {  visibility: hidden;}</style>
     <df-messenger
         chat-title="AIClub"
         agent-id="3cb41952-578d-43df-a37d-f1f12bcc12e1"
         language-code="en"></df-messenger>
     """,
     height=700, # try various values to see what works best (maybe use st.slider)
- hide_github_icon,
+
 )
 #st.markdown(hide_github_icon, unsafe_allow_html=True)
